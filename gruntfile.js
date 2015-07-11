07fd41bbc95016a43eac860d309477f4710520bc
Henri Cavalcante <hnri_mxel@hotmail.com>
2015-07-11 08:59:53 -0300

    some updates

--- gruntfile.js
+++ gruntfile.js
@@ -1,84 +1,84 @@
 'use strict';
 
 module.exports = function(grunt) {
-	var files = {
-		serverJS: ['gruntfile.js', 'server.js', 'config/**/*.js', 'app/**/*.js', 'app/**/*.js'],
-		mochaTests: ['tests/mocha/*.test.js'],
-	};
+  var files = {
+    serverJS: ['gruntfile.js', 'server.js', 'config/**/*.js', 'app/**/*.js', 'app/**/*.js'],
+    mochaTests: ['tests/mocha/*.test.js'],
+  };
 
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		watch: {
-			serverJS: {
-				files: files.serverJS,
-				tasks: ['jshint'],
-				options: {
-					livereload: true
-				}
-			},
-		},
-		jshint: {
-			all: {
-				src: files.serverJS,
-				options: {
-					jshintrc: true
-				}
-			}
-		},
-		nodemon: {
-			dev: {
-				script: 'server.js',
-				options: {
-					nodeArgs: ['--debug'],
-					ext: 'js',
-					watch: files.serverJS
-				}
-			}
-		},
-		'node-inspector': {
-			custom: {
-				options: {
-					'web-port': 1337,
-					'web-host': 'localhost',
-					'debug-port': 5858,
-					'save-live-edit': true,
-					'no-preload': true,
-					'stack-trace-limit': 50,
-					'hidden': []
-				}
-			}
-		},
-		concurrent: {
-			default: ['nodemon', 'watch'],
-			debug: ['nodemon', 'watch', 'node-inspector'],
-			options: {
-				logConcurrentOutput: true,
-				limit: 10
-			}
-		},
-		env: {
-			test: {
-				NODE_ENV: 'test'
-			},
-			secure: {
-				NODE_ENV: 'secure'
-			},
-			coverage: {
-				NODE_ENV: 'coverage',
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    watch: {
+      serverJS: {
+        files: files.serverJS,
+        tasks: ['jshint'],
+        options: {
+          livereload: true
+        }
+      },
+    },
+    jshint: {
+      all: {
+        src: files.serverJS,
+        options: {
+          jshintrc: true
+        }
       }
-		},
-		mochaTest: {
-			src: files.mochaTests,
-			options: {
-				require: './server',
-				reporter: 'spec'
-			},
-		},
+    },
+    nodemon: {
+      dev: {
+        script: 'server.js',
+        options: {
+          nodeArgs: ['--debug'],
+          ext: 'js',
+          watch: files.serverJS
+        }
+      }
+    },
+    'node-inspector': {
+      custom: {
+        options: {
+          'web-port': 1337,
+          'web-host': 'localhost',
+          'debug-port': 5858,
+          'save-live-edit': true,
+          'no-preload': true,
+          'stack-trace-limit': 50,
+          'hidden': []
+        }
+      }
+    },
+    concurrent: {
+      default: ['nodemon', 'watch'],
+      debug: ['nodemon', 'watch', 'node-inspector'],
+      options: {
+        logConcurrentOutput: true,
+        limit: 10
+      }
+    },
+    env: {
+      test: {
+        NODE_ENV: 'test'
+      },
+      secure: {
+        NODE_ENV: 'secure'
+      },
+      coverage: {
+        NODE_ENV: 'coverage',
+      }
+    },
+    mochaTest: {
+      src: files.mochaTests,
+      options: {
+        require: './server',
+        reporter: 'spec'
+      },
+    },
     mocha_istanbul: {
       coverage: {
           src: 'tests/mocha',
           options: {
-          		require: './server',
+              require: './server',
               check: {
                   lines: 20,
                   statements: 20
@@ -99,22 +99,22 @@ module.exports = function(grunt) {
       }
     },
 
-	});
+  });
 
-	require('load-grunt-tasks')(grunt);
+  require('load-grunt-tasks')(grunt);
 
-	grunt.event.on('coverage', function(lcov, done){
-		require('coveralls').handleInput(lcov, function(err){
-				if (err) {
-					return done(err);
-				}
-				done();
-			});
-	});
+  grunt.event.on('coverage', function(lcov, done){
+    require('coveralls').handleInput(lcov, function(err){
+        if (err) {
+          return done(err);
+        }
+        done();
+      });
+  });
 
-	grunt.registerTask('default', ['concurrent:default']);
-	grunt.registerTask('test', ['env:test', 'jshint', 'mochaTest']);
-	grunt.registerTask('travis', ['env:test', 'jshint', 'mocha_istanbul:coveralls']);
-	grunt.registerTask('coverage', ['env:coverage', 'mocha_istanbul:coverage']);
+  grunt.registerTask('default', ['concurrent:default']);
+  grunt.registerTask('test', ['env:test', 'jshint', 'mochaTest']);
+  grunt.registerTask('travis', ['env:test', 'jshint', 'mocha_istanbul:coveralls']);
+  grunt.registerTask('coverage', ['env:coverage', 'mocha_istanbul:coverage']);
 
 };
--- server.js
+++ server.js
@@ -1,34 +1,21 @@
 'use strict';
-/**
- * Module dependencies.
- */
 require('./config/init')();
 
 var config = require('./config/config');
 var mongoose = require('mongoose');
 var chalk = require('chalk');
 
-// Bootstrap db connection
 var db = mongoose.connect(config.db, function(err) {
-	if (err) {
-		console.error(chalk.red('Could not connect to MongoDB!'));
-		console.log(chalk.red(err));
-	}
+  if (err) {
+    console.error(chalk.red('Could not connect to MongoDB!'));
+    console.log(chalk.red(err));
+  }
 });
 
-// Init the express application
 var app = require('./config/express')(db);
 
-// Bootstrap passport config
 require('./config/passport')();
 
-// Start the app by listening on <port>
 app.listen(config.port);
 
-// Expose app
-exports = module.exports = app;
-
-// Logging initialization
 console.log(chalk.green('+Questões started on port ' + config.port));
-
-
