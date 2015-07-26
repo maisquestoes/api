b0969f4c3b64537e0e7dc2e4dd94207dd9b20078
Henri Cavalcante <hnri_mxel@hotmail.com>
2015-07-25 21:57:15 -0300

    retab in questions model

--- tests/mocha/question.server.model.test.js
+++ tests/mocha/question.server.model.test.js
@@ -3,11 +3,11 @@
 /**
  * Module dependencies.
  */
-var server = require('../../server');
-var should = require('should'),
-	mongoose = require('mongoose'),
-	User = mongoose.model('User'),
-	Question = mongoose.model('Question');
+require('../../server');
+var should = require('should');
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
+var Question = mongoose.model('Question');
 
 /**
  * Globals
@@ -18,59 +18,59 @@ var user, question;
  * Unit tests
  */
 describe('Question Model Unit Tests:', function() {
-	beforeEach(function(done) {
-		user = new User({
-			firstName: 'Full',
-			lastName: 'Name',
-			displayName: 'Full Name',
-			email: 'test@test.com',
-			username: 'username',
-			password: 'password'
-		});
+  beforeEach(function(done) {
+    user = new User({
+      firstName: 'Full',
+      lastName: 'Name',
+      displayName: 'Full Name',
+      email: 'test@test.com',
+      username: 'username',
+      password: 'password'
+    });
 
-		user.save(function() { 
-			question = new Question({
-				user: user
-			});
+    user.save(function() { 
+      question = new Question({
+        user: user
+      });
 
-			done();
-		});
-	});
+      done();
+    });
+  });
 
-	describe('Method Save', function() {
-		it('should be able to save without problems', function(done) {
-			
-			question.query = 'Quem descobriu o brasil?';
+  describe('Method Save', function() {
+    it('should be able to save without problems', function(done) {
+      
+      question.query = 'Quem descobriu o brasil?';
 
-			question.awnsers = [{
-				description:'Pedro alvares cabral.',
-				correct: true
-			},{
-				description:'xuxa',
-				correct: false
-			}];
+      question.awnsers = [{
+        description:'Pedro alvares cabral.',
+        correct: true
+      },{
+        description:'xuxa',
+        correct: false
+      }];
 
-			return question.save(function(err) {
-				should.not.exist(err);
-				done();
-			});
-		});
+      return question.save(function(err) {
+        should.not.exist(err);
+        done();
+      });
+    });
 
-		it('should be able to show an error when try to save without name', function(done) { 
-			question.query = '';
-			
+    it('should be able to show an error when try to save without name', function(done) { 
+      question.query = '';
+      
 
-			return question.save(function(err) {
-				should.exist(err);
-				done();
-			});
-		});
-	});
+      return question.save(function(err) {
+        should.exist(err);
+        done();
+      });
+    });
+  });
 
-	afterEach(function(done) { 
-		Question.remove().exec();
-		User.remove().exec();
+  afterEach(function(done) { 
+    Question.remove().exec();
+    User.remove().exec();
 
-		done();
-	});
+    done();
+  });
 });
